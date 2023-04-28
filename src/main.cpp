bc0341e87614134c98201d5b5e6665e732e5bfbf
Bradley Gatewood <btgatewood@outlook.com>
2023-04-28 06:00:57 -0400

    Add draw function; draw player

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
 
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
+
 struct App
 {
 	SDL_Renderer* renderer;
 	SDL_Window* window;
-} app;
+};
+
+
+struct Entity
+{
+	SDL_Texture* texture;  // this is a sprite
+	int x;
+	int y;
+};
+
+
+App app;
+Entity player;
+
 
 void init_SDL()
 {
@@ -18,6 +33,7 @@ void init_SDL()
 		std::cout << "Failed to initialize SDL: " << SDL_GetError() << "\n";
 	}
 
+	// TODO: make window & renderer flags config constants and assign to local int flags?
 	int window_flags = 0;
 	app.window = SDL_CreateWindow("Shooter 01", 
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,  // window pos (x,y)
@@ -38,14 +54,21 @@ void init_SDL()
 		std::cout << "Failed to create renderer: " << SDL_GetError() << "\n";
 		// exit(1);
 	}
+
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);  // png & jpg support
 }
+
 
 void cleanup()
 {
+	IMG_Quit();
+
 	SDL_DestroyRenderer(app.renderer);
 	SDL_DestroyWindow(app.window);
+
 	SDL_Quit();
 }
+
 
 void process_events()
 {
@@ -63,25 +86,78 @@ void process_events()
 	}
 }
 
+
 void begin_scene()
 {
-	SDL_SetRenderDrawColor(app.renderer, 96, 128, 255, 255);
+	SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
 	SDL_RenderClear(app.renderer);
 }
+
 
 void end_scene()
 {
 	SDL_RenderPresent(app.renderer);
 }
 
+
+SDL_Texture* load_texture(const char* file)
+{
+	SDL_Texture* texture;
+
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO,
+		"Loading %s", file);
+	texture = IMG_LoadTexture(app.renderer, file);
+
+	return texture;
+}
+
+
+void draw(SDL_Texture* texture, int x, int y)
+{
+	SDL_Rect dstrect;
+	dstrect.x = x;
+	dstrect.y = y;
+	SDL_QueryTexture(texture, nullptr, nullptr, &dstrect.w, &dstrect.h);
+	SDL_RenderCopy(app.renderer, texture, nullptr, &dstrect);
+}
+
+
+void draw(SDL_Texture* texture, int x, int y, int w, int h)
+{
+	SDL_Rect dstrect{ x, y, w, h };
+	SDL_RenderCopy(app.renderer, texture, nullptr, &dstrect);
+}
+
+
+void draw(SDL_Texture* texture, int x, int y, float scale)
+{
+	SDL_Rect dstrect;
+	SDL_QueryTexture(texture, nullptr, nullptr, &dstrect.w, &dstrect.h);
+	// calc width & height based on texture size & scale
+	dstrect.w = static_cast<int>(dstrect.w * scale);
+	dstrect.h = static_cast<int>(dstrect.h * scale);
+	// center texture dstrect to position args
+	dstrect.x = x - (dstrect.w / 2);
+	dstrect.y = y - (dstrect.h / 2);
+	SDL_RenderCopy(app.renderer, texture, nullptr, &dstrect);
+}
+
+
 int main(int argc, char* argv[])
 {
 	init_SDL();
 
+	player.x = SCREEN_WIDTH / 2;
+	player.y = SCREEN_HEIGHT / 2;
+	player.texture = load_texture("data/player.png");  // 1394x1432
+
 	while (true)
 	{
 		process_events();
+
 		begin_scene();
+		// set size to 64x64 and center texture to player position
+		draw(player.texture, player.x - 32, player.y - 32, 64, 64);
 		end_scene();
 
 		SDL_Delay(16);
